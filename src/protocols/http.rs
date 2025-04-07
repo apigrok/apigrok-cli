9e525ad9fed35e71af19a2b7e6605255ca50b16e
Dave Minter <dave@paperstack.com>
2025-04-07 19:42:23 +0200

    feat: add selection of verbose output content

--- src/protocols/http.rs
+++ src/protocols/http.rs
@@ -1,5 +1,7 @@
-use super::*;
-use reqwest::Client;
+use crate::protocols::{ApiProtocol, ApiRequest, ApiResponse, Protocol};
+use async_trait::async_trait;
+use reqwest::{Client, Version};
+use std::error::Error;
 use std::time::Instant;
 
 pub struct HttpClient {
@@ -14,7 +16,7 @@ pub enum HttpVersion {
 
 #[async_trait]
 impl ApiProtocol for HttpClient {
-    async fn fetch(&self, url: &str) -> Result<ApiResponse, Box<dyn Error>> {
+    async fn fetch(&self, url: &str) -> Result<(ApiRequest, ApiResponse), Box<dyn Error>> {
         let client = match self.version {
             HttpVersion::Http1 => Client::builder().http1_only().build()?,
             // HttpVersion::Http2 => Client::builder().http2_prior_knowledge().build()?,
@@ -24,7 +26,19 @@ impl ApiProtocol for HttpClient {
         };
 
         let start = Instant::now();
-        let response = client.get(url).send().await?;
+
+        let request = client.get(url).build()?;
+        let request_headers: Vec<(String, String)> = request
+            .headers()
+            .iter()
+            .map(|(k, v)| (k.to_string(), v.to_str().unwrap_or("").to_string()))
+            .collect();
+
+        let request_version = format!("{:?}", &request.version());
+        let request_path = (&request.url().path()).to_string();
+
+        let response = client.execute(request).await?;
+
         let path = response.url().path().to_string();
         let duration = start.elapsed();
         let status = response.status().as_u16();
@@ -38,20 +52,28 @@ impl ApiProtocol for HttpClient {
 
         let body = response.bytes().await?.to_vec();
 
-        Ok(ApiResponse {
-            path: path,
-            protocol: match self.version {
-                HttpVersion::Http1 => Protocol::Http1,
-                // HttpVersion::Http2 => Protocol::Http2,
-                // HttpVersion::Http3 => Protocol::Http3,
+        Ok((
+            ApiRequest {
+                headers: Some(request_headers),
+                method: "GET".to_string(),
+                path: request_path,
+                version: request_version,
             },
-            status: Some(status),
-            headers: Some(headers),
-            body: Some(body),
-            version: format!("{}", version_to_string(version)),
-            ip: ip,
-            duration: duration,
-        })
+            ApiResponse {
+                path: path,
+                protocol: match self.version {
+                    HttpVersion::Http1 => Protocol::Http1,
+                    // HttpVersion::Http2 => Protocol::Http2,
+                    // HttpVersion::Http3 => Protocol::Http3,
+                },
+                status: Some(status),
+                headers: Some(headers),
+                body: Some(body),
+                version: format!("{}", version_to_string(version)),
+                ip: ip,
+                duration: duration,
+            },
+        ))
     }
 }
 
