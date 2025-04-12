3dd3550bb87b55fd12d837c20bce6907c3b712dc
Haris Kljajic <haris@myrstack.tech>
2025-04-12 22:15:30 +0200

    feat: add Daves grokking and adaption to https vs http

--- src/protocols/http.rs
+++ src/protocols/http.rs
@@ -4,18 +4,16 @@ use std::sync::Arc;
 use std::vec;
 
 use http_body_util::Empty;
-use hyper::body::{Bytes, Incoming};
+use hyper::body::Bytes;
 use hyper::client::conn::http2;
-use hyper::{Request, Response, header};
+use hyper::{Request, header};
+use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::rt::{TokioExecutor, TokioIo};
-use rustls::RootCertStore;
-use rustls::pki_types::{CertificateDer, ServerName};
+use rustls::pki_types::ServerName;
 use rustls_native_certs::load_native_certs;
 use tokio::net::TcpStream;
 use tokio_rustls::{TlsConnector, rustls::ClientConfig};
-use tokio_util::compat::TokioAsyncReadCompatExt;
-
-use crate::clients;
+use url::Url;
 
 pub struct HttpClient {
     pub version: HttpVersion,
@@ -30,59 +28,128 @@ pub enum HttpVersion {
 #[async_trait]
 impl ApiProtocol for HttpClient {
     async fn fetch(&self, url: &str) -> Result<ApiResponse, Box<dyn Error>> {
-        let domain = "myrstack.tech";
-
-        let mut root_store = rustls::RootCertStore::empty();
-        for cert in load_native_certs().expect("Could not load platform certificates") {
-            root_store.add(cert)?;
-        }
-
-        let mut config = ClientConfig::builder_with_protocol_versions(&[&rustls::version::TLS13])
-            .with_root_certificates(root_store)
-            .with_no_client_auth();
-
-        // Configure ALPN protocols (order matters!)
-        config.alpn_protocols = vec![b"h2".to_vec()]; // Prefer HTTP/2
-
-        let connector = TlsConnector::from(Arc::new(config));
-        let server_name = ServerName::try_from(domain)?;
+        let parsed_url = Url::parse(url)?;
+        let scheme = parsed_url.scheme();
+        let host = parsed_url.host_str().ok_or("Invalid host")?;
+        let domain = parsed_url.domain().ok_or("Invalid domain")?.to_string();
+        let port = parsed_url
+            .port_or_known_default()
+            .unwrap_or_else(|| if scheme == "https" { 443 } else { 80 });
 
         // 1. Open TCP connection
-        let tcp = TcpStream::connect((domain, 443)).await?;
-        // 2. Wrap with TLS using ALPN
-        let tls = connector.connect(server_name, tcp).await?;
+        let tcp = TcpStream::connect((domain.clone(), port)).await?;
+        println!("Connected to {}:{} via {}", host, port, scheme);
+        println!("{:?}", domain);
 
-        let (_, session) = tls.get_ref();
-        if session.alpn_protocol() != Some(b"h2") {
-            return Err("Server didn't negotiate HTTP/2".into());
-        }
+        println!("Local address: {}", tcp.local_addr()?);
+        println!("Peer address: {}", tcp.peer_addr()?);
+        println!("Socket TTL: {}", tcp.ttl()?);
+        println!("Nodelay setting: {}", tcp.nodelay()?);
 
-        // 4. Perform HTTP/2 handshake
-        let (mut sender, conn) = http2::Builder::new(TokioExecutor::new())
-            .initial_stream_window_size(65535)
-            .initial_connection_window_size(1_048_576)
-            .max_frame_size(16_384) // Common server-friendly size
-            .handshake(TokioIo::new(tls))
-            .await?;
+        let server_name = ServerName::try_from(domain.clone())?;
 
-        // 5. Spawn connection driver
-        tokio::spawn(async move {
-            if let Err(err) = conn.await {
-                eprintln!("Connection failed: {:?}", err);
+        let io = match scheme {
+            "https" => {
+                // 2. Wrap with TLS using ALP
+                let mut root_store = rustls::RootCertStore::empty();
+                for cert in load_native_certs().expect("Could not load platform certificates") {
+                    root_store.add(cert)?;
+                }
+
+                let mut tls_config =
+                    ClientConfig::builder_with_protocol_versions(&[&rustls::version::TLS13])
+                        .with_root_certificates(root_store)
+                        .with_no_client_auth();
+                // Configure ALPN protocols (order matters!)
+                tls_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()]; // Prefer HTTP/2
+
+                let connector = TlsConnector::from(Arc::new(tls_config));
+                let mut tls = connector.connect(server_name, tcp).await?;
+
+                let (_, client_connection) = tls.get_mut();
+                // let foo = client_connection.peer_certificates();
+                match client_connection.peer_certificates() {
+                    Some(certs) => {
+                        for der_cert in certs {
+                            // These are DER-encoded bytes (https://datatracker.ietf.org/doc/html/rfc5280)
+                            let raw_cert = &der_cert.to_vec();
+                            let (_, decoded_cert) = x509_parser::parse_x509_certificate(raw_cert)?;
+
+                            println!("Certificate Version: {}", &decoded_cert.version);
+                            println!("Certificate Issuer: {}", &decoded_cert.issuer);
+                            println!("Certificate Subject: {}", &decoded_cert.subject);
+                            let cert_validity = &decoded_cert.validity;
+                            let start = &cert_validity.not_before;
+                            let end = &cert_validity.not_after;
+                            println!("Certificate Validity: From {} until {}", start, end);
+                            println!("...");
+                        }
+                    }
+                    None => {}
+                }
+
+                let (_, session) = tls.get_ref();
+                if session.alpn_protocol() != Some(b"h2") {
+                    return Err("Server didn't negotiate HTTP/2".into());
+                }
+
+                let (mut sender, conn) = http2::Builder::new(TokioExecutor::new())
+                    .initial_stream_window_size(65535)
+                    .initial_connection_window_size(1_048_576)
+                    .max_frame_size(16_384) // Common server-friendly size
+                    .handshake(TokioIo::new(tls))
+                    .await?;
+
+                // 5. Spawn connection driver
+                tokio::spawn(async move {
+                    if let Err(err) = conn.await {
+                        eprintln!("Connection failed: {:?}", err);
+                    }
+                });
+
+                // 6. Send HTTP/2 request
+                let req: Request<Empty<Bytes>> = Request::builder()
+                    .uri(format!("{}://{}", scheme, domain.clone()))
+                    .header(header::HOST, parsed_url.domain().unwrap_or_default())
+                    .header(header::USER_AGENT, "my-client/0.1.0")
+                    .header(header::ACCEPT, "*/*")
+                    .body(Empty::new())?;
+
+                let res = sender.send_request(req).await?;
+
+                println!("Status: {}", res.status())
             }
-        });
+            "http" => {
+                let (mut sender, conn) = http2::Builder::new(TokioExecutor::new())
+                    .initial_stream_window_size(65535)
+                    .initial_connection_window_size(1_048_576)
+                    .max_frame_size(16_384)
+                    .handshake(TokioIo::new(tcp))
+                    .await?;
 
-        // 6. Send HTTP/2 request
-        let req: Request<Empty<Bytes>> = Request::builder()
-            .uri(format!("https://{}/", domain))
-            .header(header::HOST, domain)
-            .header(header::USER_AGENT, "my-client/0.1.0")
-            .header(header::ACCEPT, "*/*")
-            .body(Empty::new())?;
+                tokio::spawn(async move {
+                    if let Err(err) = conn.await {
+                        eprintln!("Connection failed: {:?}", err);
+                    }
+                });
 
-        let res = sender.send_request(req).await?;
-        println!("Response: {}", res.status());
-        println!("Headers: {:#?}", res.headers());
+                println!("{}", format!("{}://{}", scheme, domain.clone()));
+
+                let req: Request<Empty<Bytes>> = Request::builder()
+                    .uri(format!("{}://{}", scheme, domain.clone()))
+                    .header(header::HOST, parsed_url.domain().unwrap_or_default())
+                    .header(header::USER_AGENT, "my-client/0.1.0")
+                    .header(header::ACCEPT, "*/*")
+                    .body(Empty::new())?;
+
+                let res = sender.send_request(req).await?;
+
+                println!("Status: {}", res.status())
+            }
+            _ => {
+                println!("Unsupported scheme: {}", scheme);
+            }
+        };
 
         // // Parse our URL...
         // let url = url.parse::<hyper::Uri>()?;
