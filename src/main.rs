d275c77c1945a232ed6b7a7e2823c327d19ef367
Haris Kljajic <haris@myrstack.tech>
2025-04-19 21:59:03 +0200

    feat: fix cleaner commands

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 
 use clap::{CommandFactory, Parser, Subcommand, ValueEnum};
 use clap_complete::{Shell, generate};
+use hyper::Method;
 use protocols::{ApiProtocol, ApiResponse, Protocol};
 use std::io;
 
@@ -13,26 +14,51 @@ use std::io;
 #[command(about = "A CLI tool to explore and understand APIs", long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
+
+    /// Shortcut for GET via HTTP/1.x
+    #[arg()]
+    url: Option<String>,
+
+    /// Set the verbosity level
+    #[arg(short = 'v', long, value_enum, default_value_t = Verbosity::Normal, global = true)]
+    verbosity: Verbosity,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Fetch the content available at a specified url
-    Fetch {
+    /// Perform a request using HTTP/1.x
+    Http {
+        #[arg(value_enum)]
+        method: Method,
+
         url: String,
 
-        #[arg(short, long, value_enum, default_value_t = Protocol::Http1)]
-        protocol: Protocol,
+        #[arg(
+            long,
+            help = "Attempt to upgrade to HTTP/2 over cleartext (h2c) after initial HTTP/1.x connection"
+        )]
+        h2c: bool,
+    },
 
-        #[arg(short('v'), long, value_enum, default_value_t = Verbosity::Normal)]
-        verbosity: Verbosity,
+    /// Perform a request using HTTP/2
+    Http2 {
+        #[arg(value_enum)]
+        method: Method,
+
+        url: String,
     },
-    /// Generate autocompletion scripts for various shells
-    Completion {
-        /// The shell for which autocompletion should be generated (e.g. bash)
-        shell: Shell,
+
+    /// Perform a gRPC request
+    Grpc {
+        #[arg(value_enum)]
+        method: Method,
+
+        url: String,
     },
+
+    /// Generate autocompletion scripts
+    Completion { shell: Shell },
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
@@ -48,30 +74,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Fetch {
-            url,
-            protocol,
-            verbosity,
-        } => {
-            let client = match protocol {
-                Protocol::Http1 => Box::new(protocols::http::HttpClient {
-                    version: protocols::http::HttpVersion::Http1,
-                }) as Box<dyn ApiProtocol>,
-                _ => unimplemented!("Protocol not yet implemented"),
-            };
-
-            let response = client.fetch(&url).await?;
-
-            render_response(&response, verbosity)?;
+        Some(Commands::Http { method, url }) => {
+            // TODO: http/1.x call
+            println!("Performing http1.x {:?} to {}", method, url);
         }
-        Commands::Completion { shell } => {
-            let command = &mut Cli::command();
-            generate(
-                shell,
-                command,
-                command.get_name().to_string(),
-                &mut io::stdout(),
-            );
+
+        Some(Commands::Http2 { method, url }) => {
+            // TODO: http/2 call
+            println!("Performing http2 {:?} to {}", method, url);
+        }
+
+        Some(Commands::Grpc { method, url }) => {
+            // TODO: grpc call
+            println!("Performing gRPC {:?} to {}", method, url);
+        }
+
+        Some(Commands::Completion { shell }) => {
+            let cmd = &mut Cli::command();
+            generate(shell, cmd, cmd.get_name().to_string(), &mut io::stdout());
+        }
+
+        None => {
+            if let Some(url) = cli.url {
+                // Default: GET via HTTP/1.1
+                let client: Box<dyn ApiProtocol> = Box::new(protocols::http::HttpClient {
+                    version: protocols::http::HttpVersion::Http1,
+                });
+                let response = client.fetch(&url).await?;
+
+                render_response(&response, cli.verbosity)?;
+            } else {
+                eprintln!("No command or URL provided. Try `--help`.");
+            }
         }
     }
 
